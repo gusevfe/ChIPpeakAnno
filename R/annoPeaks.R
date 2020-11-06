6f0b32edefb024653dec24dee447b05919e08dd5
Fedor Gusev <gusevfe@gmail.com>
2020-11-06 18:57:03 +0300

    Disable seqlevels check.

--- R/annoPeaks.R
+++ R/annoPeaks.R
@@ -75,7 +75,6 @@ annoPeaks <- function(peaks, annoData,
     }
     stopifnot(inherits(peaks, "GRanges"))
     stopifnot(inherits(annoData, c("annoGR", "GRanges")))
-    stopifnot(length(intersect(seqlevelsStyle(peaks),seqlevelsStyle(annoData)))>0)
     stopifnot(length(bindingRegion)==2)
     stopifnot(bindingRegion[1]<=0 && bindingRegion[2]>=1)
     if(ignore.peak.strand){
